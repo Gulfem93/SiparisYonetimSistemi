91bbc7423af8c2bcd49ddb9d4f25257f1cbcd344
Gülfem IŞIK <35203615+Gulfem93@users.noreply.github.com>
2026-04-02 02:43:00 +0300

    first commit

---
+++ src/main/java/com/siparisYonetim/SiparisYonetimSistemi/SiparisYonetimSistemiApplication.java
@@ -0,0 +1,13 @@
+package com.siparisYonetim.SiparisYonetimSistemi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiparisYonetimSistemiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiparisYonetimSistemiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siparisYonetim/SiparisYonetimSistemi/SiparisYonetimSistemiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siparisYonetim.SiparisYonetimSistemi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiparisYonetimSistemiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
