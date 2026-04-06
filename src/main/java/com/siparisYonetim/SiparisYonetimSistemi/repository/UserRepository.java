cbdd6279578bec920462815a7c1cdb0995badf5b
Gülfem IŞIK <35203615+Gulfem93@users.noreply.github.com>
2026-04-06 16:23:23 +0300

    make Login

---
+++ src/main/java/com/siparisYonetim/SiparisYonetimSistemi/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package com.siparisYonetim.SiparisYonetimSistemi.repository;
+
+public interface UserRepository{
+}
---
+++ src/main/java/com/siparisYonetim/SiparisYonetimSistemi/security/UserSecurity.java
@@ -0,0 +1,7 @@
+package com.siparisYonetim.SiparisYonetimSistemi.security;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserSecurity {
+}
