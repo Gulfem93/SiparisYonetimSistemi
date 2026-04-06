d3b67eb33b080e5cdfae84b4bb0ffedf2ce8cf3a
Gülfem IŞIK <35203615+Gulfem93@users.noreply.github.com>
2026-04-06 16:20:49 +0300

    make Login

---
+++ src/main/java/com/siparisYonetim/SiparisYonetimSistemi/config/SpringSecurity.java
@@ -0,0 +1,5 @@
+package com.siparisYonetim.SiparisYonetimSistemi.config;
+
+
+public class SpringSecurity {
+}
---
+++ src/main/java/com/siparisYonetim/SiparisYonetimSistemi/controller/HomeController.java
@@ -0,0 +1,7 @@
+package com.siparisYonetim.SiparisYonetimSistemi.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+}
