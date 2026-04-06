8362bff96f36074663d891f4cc08943ed21e0dc9
Gülfem IŞIK <35203615+Gulfem93@users.noreply.github.com>
2026-04-06 23:23:39 +0300

    make Login

--- src/main/java/com/siparisYonetim/SiparisYonetimSistemi/controller/LoginController.java
+++ src/main/java/com/siparisYonetim/SiparisYonetimSistemi/controller/LoginController.java
@@ -1,17 +1,18 @@
 package com.siparisYonetim.SiparisYonetimSistemi.controller;
 
 import com.siparisYonetim.SiparisYonetimSistemi.constant.ControllerConstant;
+import com.siparisYonetim.SiparisYonetimSistemi.data.UserData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(ControllerConstant.HOME)
-public class HomeController {
+@RequestMapping(ControllerConstant.LOGIN)
+public class LoginController {
     @GetMapping
-    public String getHome(Model model) {
-        model.addAttribute("home", "Home");
-        return "home";
+    public String getLogin(Model model) {
+        model.addAttribute("loginForm", new UserData());
+        return "login";
     }
 }
