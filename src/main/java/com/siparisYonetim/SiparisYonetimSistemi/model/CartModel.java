6f5844c866b5821a8e10a0c18df0c23ae73be86a
Gülfem IŞIK <35203615+Gulfem93@users.noreply.github.com>
2026-04-06 16:16:31 +0300

    make Login

--- src/main/java/com/siparisYonetim/SiparisYonetimSistemi/model/CartModel.java
+++ src/main/java/com/siparisYonetim/SiparisYonetimSistemi/model/CartModel.java
@@ -1,21 +1,33 @@
 package com.siparisYonetim.SiparisYonetimSistemi.model;
 
+import com.siparisYonetim.SiparisYonetimSistemi.data.CustomerData;
 import com.siparisYonetim.SiparisYonetimSistemi.data.ProductData;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
-@Table(name = "BASKET")
 @Getter
 @Setter
-
-public class BasketModel {
+@ToString
+@NoArgsConstructor
+@Table(name = "CART")
+public class CartModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY )
+    @JoinColumn(name = "customer_id")
+    private CustomerModel customer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id")
     private ProductModel product;
+
+    private Long price;
+    private String date;
+    private String note;
 }
